fa06fc705e253f5c0fde1989fc6a02e8a3fcc130
Pasbatron <131593509+pasbatron@users.noreply.github.com>
2024-06-26 16:48:53 +0700

    Update main.php

--- Project_Energy/main.php
+++ Project_Energy/main.php
@@ -1,6 +1,5 @@
 <?php include 'getConnect.php'; ?>
-<?php include 'template_base.php'; ?>
-
+<?php include 'getTemplateBase.php'; ?>
 
 
 <html lang="en">
@@ -8,13 +7,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="images/icon_pt_otics_1.png" type="image/x-icon">
-    <link rel="stylesheet" href="style.css">
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <link rel="stylesheet" href="src/style.css">
     <title>Monitoring Energy</title>
 </head>
 
 
 <body>
+
     <nav class="navbar">
         <div class="container-navbar">
             <div class="logo"><a href="main.php">PT.Otics Indonesia Monitoring Energy</a></div>
@@ -23,12 +22,9 @@
             </ul>
         </div>
     </nav>
-
-    <h2>Realtime Monitoring</h2>
     <br><hr>
-
-
-
+    <h2>Realtime Monitoring Energy</h2>
+    
     <div class="layout-main-1">
         <div class="item-1">
             <table>
@@ -37,20 +33,17 @@
                     <th>Nama Power Meter</th>
                     <th>Date</th>
                     <th>Time</th>
-                    <th>Data Akhir Wh Meter</th>
                 </tr>
-
                 <?php
                     $sql = "SELECT * FROM table_all_energy WHERE name_power_meter = 'POWER METER 200V' ORDER BY id DESC LIMIT 1";
                     $result = $conn->query($sql);
                     if ($result->num_rows > 0) {
                         while($row = $result->fetch_assoc()) {
                             echo "<tr>
-                                    <td rowspan='3'>{$row['name_panel']}</td>
+                                    <td rowspan='3'>DISTRIBUTION PANEL ALL OTICS</td>
                                     <td>{$row['name_power_meter']}</td>
                                     <td>{$row['date']}</td>
                                     <td>{$row['time']}</td>
-                                    <td>{$row['data_kwh_meter']}</td>
                                 </tr>";
                             $data_kwh_meter_a = (int)$row['data_kwh_meter'];
                         }
@@ -79,7 +72,6 @@
                         }
                     } 
 
-
                     $total_pm200_shift_satu = $data_kwh_meter_a - $data_kwh_meter_b;
                     $total_pm200_shift_dua = $data_kwh_meter_c - $data_kwh_meter_d;
 
@@ -91,13 +83,10 @@
                                     <td>{$row['name_power_meter']}</td>
                                     <td>{$row['date']}</td>
                                     <td>{$row['time']}</td>
-                                    <td>{$row['data_kwh_meter']}</td>
                                 </tr>";
                                 $data_kwh_meter_e = (int)$row['data_kwh_meter'];
                         }
                     } 
-
-
 
                     $sql = "SELECT * FROM table_all_energy WHERE name_power_meter = 'POWER METER 220V' and time < '07:10:00' ORDER BY id DESC LIMIT 1";
                     $result = $conn->query($sql);
@@ -127,16 +116,11 @@
                     }
                     $total_pm220_shift_satu = $data_kwh_meter_e - $data_kwh_meter_f;
                     $total_pm220_shift_dua = $data_kwh_meter_g - $data_kwh_meter_h;
-
-
                 ?>
             </table>
         </div>
 
 
-        <div class="item-5">
-            
-        </div>
         <div class="item-1">
             <div class="kwh-meter">
                 <div class="kwh-heading">
@@ -160,6 +144,7 @@
                 </div>
             </div>
         </div>
+
         <div class="item-3">
             <div class="kwh-meter">
                 <div class="kwh-heading">
@@ -171,6 +156,7 @@
                 </div>
             </div>
         </div>
+
         <div class="item-4">
             <div class="kwh-meter">
                 <div class="kwh-heading">
@@ -183,17 +169,61 @@
             </div>
         </div>
     </div>
-    <br><hr>
-
-
-
     <!-- section report -->
-    <div class="section-report">
+    <br><hr>
+    <h2>History Consumption Energy</h2>
+    <br><hr>
+    <div class="layout-main-2">
         <?php header_table(); ?>
-    </div>
-                    
+        <?php  
+            // DPCHAB
+            $dataGetDataDPCHABPM200 = getDataDPCHABPM200();
+            $dataGetDataDPCHABPM200Date = getDataDPCHABPM200Date();
+            $dataGetDataDPCHABPM200time = getDataDPCHABPM200Time();
+            $dataGetDataDPCHABPM220 = getDataDPCHABPM220();
+            $dataGetDataDPCHABPM220Date = getDataDPCHABPM220Date();
+            $dataGetDataDPCHABPM220time = getDataDPCHABPM220Time();
+            
 
-    <?php $conn->close(); ?> 
-    <script src="script.js"></script>
+            $dataGetDataDPCHCDPM200 = getDataDPCHCDPM200();
+            $dataGetDataDPCHCDPM200Date = getDataDPCHCDPM200Date();
+            $dataGetDataDPCHCDPM200time = getDataDPCHCDPM200Time();
+            $dataGetDataDPCHCDPM220 = getDataDPCHCDPM220();
+            $dataGetDataDPCHCDPM220Date = getDataDPCHCDPM220Date();
+            $dataGetDataDPCHCDPM220time = getDataDPCHCDPM220Time();
+            
+            echo "<tr>";
+            echo "<td> $dataGetDataDPCHABPM200Date</td>";
+            echo "<td>DP CAMP HOUSING A&B</td>";
+            echo "<td>$dataGetDataDPCHABPM200</td>";
+
+
+            echo "<td>Generated Data 2</td>";
+            echo "<td>Generated Data 3</td>";
+            echo "<td>Generated Data 4</td>";
+            echo "</tr>";
+
+
+            echo "<tr>";
+            echo "<td> $dataGetDataDPCHCDPM200Date</td>";
+            echo "<td>DP CAMP HOUSING C&D</td>";
+            echo "<td>$dataGetDataDPCHCDPM200</td>";
+
+
+            echo "<td>Generated Data 2</td>";
+            echo "<td>Generated Data 3</td>";
+            echo "<td>Generated Data 4</td>";
+            echo "</tr>";
+        ?>
+        <?php echo "</table>"; ?>
+    
+    </div>
+    <!-- <br><br><br><br> -->
+    <h4>DP = DISTRIBUTION PANEL</h4>
+
+
+    <script src="src/script.js"></script>
+
+    
 </body>
 </html>
