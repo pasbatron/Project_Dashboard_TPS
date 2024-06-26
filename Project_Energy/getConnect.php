4c63da37bb3c5c95b98ba1d83474c59b587307d2
Pasbatron <131593509+pasbatron@users.noreply.github.com>
2024-06-26 07:21:46 +0700

    Create getConnect.php

---
+++ Project_Energy/getConnect.php
@@ -0,0 +1,18 @@
+
+<?php
+    $servername = "localhost:3307";
+    $username = "root";
+    $password = "";
+    $dbname = "database_tps_energy";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+
+?>
+
